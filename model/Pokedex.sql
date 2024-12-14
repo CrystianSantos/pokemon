ad1f6fe1a2970ef3af86d2593f380df927d92345
Rynilan <ilanvitorsantos@gmail.com>
2024-12-14 02:13:59 -0300

    code: adicionada nova estrutura do banco mais atualização das descrições dos pokemons.
    Signed-off-by: Rynilan <ilanvitorsantos@gmail.com>

--- model/Pokedex.sql
+++ model/Pokedex.sql
@@ -1,12 +1,18 @@
 create database Pokedex;
+create table Pokedex.tb_tipos(
+	id int (10) not null auto_increment primary key ,
+	nome varchar(10) not null
+);
 create table Pokedex.tb_pokemons (
 	id int(10) not null auto_increment primary key,
 	nome varchar(50) not null,
-	tipo_1 varchar(10) not null,
-	tipo_2 varchar(10),
+	tipo_1 int(10) not null,
+	tipo_2 int(10),
 	foto varchar(100) not null,
 	defesa int not null,
 	hp int not null,
 	ataque int not null,
 	descrição varchar(500)
+	foreign key (tipo_2) references tb_tipos(id),
+	foreign key (tipo_1) references tb_tipos(id)
 );
